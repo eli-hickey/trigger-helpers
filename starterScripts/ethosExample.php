f29bd88cb441d6135b0f0947b4958feb629cfe00
Hickey, Eli <Eli.Hickey@ellucian.com>
2020-09-30 15:11:07 -0500

    updatel

--- starterScripts/ethosExample.php
+++ starterScripts/ethosExample.php
@@ -3,12 +3,12 @@ require_once "./classes/ethosFunctions.php";
 require_once "./classes/helperFunctions.php";
 require_once "./classes/workflowFunctions.php";
 //Start Trigger Code (Generator Instruction do not remove) &*^EWFSTART:
+
+
 $response = eeGetEthosDataModelByFilter("buildings","");
 print json_encode($response,JSON_PRETTY_PRINT);
 $atat_someCaseVar = "foo";
 $ateq_someCaseVarGrid = "foo";
 
-
-print "end";
-
 //:&*^EWFEND End Trigger Code (Generator Instruction do not remove)
+print "end";
